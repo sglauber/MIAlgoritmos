6753ec35b800847e8a456b30594b7c549adf5f83
Glauber <glauber.silva14@gmail.com>
2018-05-14 18:21:36 -0300

    O aninhamento de estruturas foi alterado. Novos protótipos adicionados.

--- 2Prob_ConcursoInovaTec_20181/functions_headers.h
+++ 2Prob_ConcursoInovaTec_20181/functions_headers.h
@@ -1,14 +1,37 @@
 #ifndef FUNCTIONS_HEADERS_H
 #define FUNCTIONS_HEADER_H
 
-typedef struct {
-  union {
-    char nome_projeto[30];
-    char nome_equipe[30];
-  }nomes;
+#ifdef _WIN32 //Se for Windows ambas arquiteturas x86 e x64
+  #define CLEAR system("cls");
+#else //In any other OS
+  #define CLEAR system("clear");
+#endif
+#define MAX 20
+#include <stdio.h>
+
+typedef struct Projeto {
+  char nome_projeto[MAX];
   int  codigo_projeto;
-  char* cp;
-  char  cbuff[20];
+  int notas_jurados[5][5];
+  char categoria_projeto[MAX];
+}tipoProjeto;
+
+/*  As notas compreendem valores de 0 a 10. Cada coluna do vetor irá corresponder a uma nota (critério do jurado) e cada linha corresponde a um jurado.
+    Nível de Organização da Equipe na posição [0]
+    Estratégia de Venda [1]
+    Grau de usabilidade do produto [2]
+    Nível de maturidade da área que pretendem atuar [3];
+    Grau de inovação do produto [4] */
+
+typedef struct Equipes {
+  tipoProjeto equipeProjeto;
+  char nome_equipe[MAX];
+  char nome_participante[MAX];
+}tipoEquipe;
+
+typedef struct Conteudo {
+  tipoEquipe Equipe;
+  tipoProjeto Projeto;
 }tipoConteudo;
 
 /* Célula da lista
@@ -21,14 +44,20 @@ typedef struct Lista {
   struct Lista *Anterior;
 }tipoCelula;
 
-//void criaCelula();
-int menuSelecao();
-tipoCelula *criaCelula(tipoConteudo Conteudo);//deve receber uma struct como parametro
-void cadastraProposta();
+int selecionaMenu();
+tipoCelula *criaCelula(tipoProjeto Projeto);
+void selecionaOpcaoCadastro();
+void cadastraEquipe();
+tipoEquipe lerEquipe();
+void cadastraProjeto();
+tipoProjeto lerProjeto();
 tipoCelula* PercorrerLista(tipoCelula* Percorrer, int count);
-tipoConteudo lerCadastro();
-void mostrarLista();
-int buscaPorCodigo(tipoCelula **Celula, int *codigo);
-void criaArquivo();
+tipoProjeto lerCadastro();
+void validaCategoria();
+int escolheCategoria();
+int validaCodigo();
+void exibirLista();
+int buscaPorCodigo(tipoProjeto CodeFind);
+FILE *abreArquivo(char *nome_arquivo);
 void lerArquivo();
  #endif
